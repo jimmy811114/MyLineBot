e225e102689c57e4b2a26fb6722a0c1de42a4c9e
Jimmy <Jimmy@192.168.170.215>
2017-08-13 23:48:25 +0800

    Jimmy_update

--- wallet.js
+++ wallet.js
@@ -36,6 +36,28 @@ exports.set_Money = function (money, status, event) {
     sendMsg(event, msg + ':' + money);
 };
 
+//重新計算
+exports.set_Money = function (money, status, event) {
+    var connection = mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'x22122327',
+        database: 'wallet'
+    });
+    connection.connect();
+    var addSql = 'drop table money';
+    //建立
+    connection.query(addSql, function (err, result) {
+        if (err) {
+            console.log('[INSERT ERROR] - ', err.message);
+            return;
+        }
+
+    });
+    connection.end();
+    sendMsg(event, '重新計算');
+};
+
 //顯示金額
 exports.show_Money = function (event) {
     var connection = mysql.createConnection({
@@ -47,8 +69,8 @@ exports.show_Money = function (event) {
     connection.connect();
     var sql = 'SELECT sum(money) as total FROM money where type = 1';
     var sql0 = 'SELECT sum(money) as total FROM money where type = 0';
-    var mon_1 = 0;
-    var mon_0 = 0;
+    var mon_1;
+    var mon_0;
     connection.query(sql, function (err, result, fields) {
         if (err) {
             console.log('[SELECT ERROR] - ', err.message);
@@ -58,7 +80,7 @@ exports.show_Money = function (event) {
         console.log('--------------------------SELECT----------------------------');
         console.log(result);
         console.log('------------------------------------------------------------\n\n');
-        mon_1 = fields[0].total;
+        mon_1 = result[0].total;
     });
     connection.query(sql0, function (err, result, fields) {
         if (err) {
@@ -69,11 +91,13 @@ exports.show_Money = function (event) {
         console.log('--------------------------SELECT----------------------------');
         console.log(result);
         console.log('------------------------------------------------------------\n\n');
-        mon_0 = fields[0].total;
+        mon_0 = result[0].total;
+        var t_mon = mon_1 + mon_0;
+        var msg = '總共存:' + mon_1 + '\n' + '總共花:' + mon_0 + '\n餘額:' + t_mon;
+        sendMsg(event, msg);
     });
     connection.end();
-    var msg = '總共存:' + mon_1 + '\n' + '總共花:' + mon_0;
-    sendMsg(event, msg + ':' + msg);
+
 };
 
 //傳送訊息
